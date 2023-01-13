a3c77d31260a95e0fd1449a8dfdd46047cde9569
Jonathan Labrum <jonathan.d.labrum@gmail.com>
2023-01-12 19:29:20 -0600

    Updated constants and rename files

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,5 +15,30 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    
+    public static final int CONTROLLER_PORT = 0;
+
+    public static final int DRIVE_LEFT_FRONT_MOTOR = 1;
+    public static final int DRIVE_LEFT_BACK_MOTOR = 2;
+    public static final int DRIVE_RIGHT_FRONT_MOTOR = 3;
+    public static final int DRIVE_RIGHT_BACK_MOTOR = 4;
+    
+    public static final int LEFT_STICK_X = 0;
+    public static final int LEFT_STICK_Y = 1;
+    public static final double LEFT_TRIGGER = 2;
+    public static final double RIGHT_TRIGGER = 3;
+    public static final double RIGHT_STICK_X = 4;
+    public static final double RIGHT_STICK_Y = 5;
+
+    public static final int BUTTON_A = 1;
+    public static final int BUTTON_B = 2;
+    public static final int BUTTON_X = 3;
+    public static final int BUTTON_Y = 4;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int BACK_BUTTON = 7;
+    public static final int START_BUTTON = 8;
+    public static final int LEFT_STICK_BUTTON = 9;
+    public static final int RIGHT_STICK_BUTTON = 10;
   }
 }
