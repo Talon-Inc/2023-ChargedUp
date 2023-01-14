c0649c332bc27fd16a42b6c157d95d8a24500381
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2023-01-14 13:41:30 -0600

    Updated Drive.java and renamed DriveCommand to Drive

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,10 +4,27 @@
 
 package frc.robot.subsystems;
 
+import static frc.robot.Constants.OperatorConstants.*;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
+  private final PWMSparkMax m_leftfrontDrive = new PWMSparkMax(DRIVE_LEFT_FRONT_MOTOR);
+  private final PWMSparkMax m_leftrearDrive = new PWMSparkMax(DRIVE_LEFT_BACK_MOTOR);
+  private final MotorControllerGroup m_leftDrive = new MotorControllerGroup(m_leftfrontDrive, m_leftrearDrive);
+
+  private final PWMSparkMax m_rightfrontDrive = new PWMSparkMax(DRIVE_RIGHT_FRONT_MOTOR);
+  private final PWMSparkMax m_rightrearDrive = new PWMSparkMax(DRIVE_RIGHT_BACK_MOTOR);
+  private final MotorControllerGroup m_rightDrive = new MotorControllerGroup(m_rightfrontDrive, m_rightrearDrive);
+
+  private final DifferentialDrive m_RoboDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
+  private final Timer m_Timer = new Timer();
+  
   /** Creates a new DriveTrain. */
   public DriveTrain() {
 
--- src/main/java/frc/robot/subsystems/DriveTrain_old.java
+++ src/main/java/frc/robot/subsystems/DriveTrain_old.java
@@ -9,19 +9,21 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 public class DriveTrain_old {
     private final PWMSparkMax m_leftfrontDrive = new PWMSparkMax(0);
     private final PWMSparkMax m_leftrearDrive = new PWMSparkMax(1);
-    MotorControllerGroup m_leftDrive = new MotorControllerGroup(m_leftfrontDrive, m_leftrearDrive);
+    private final MotorControllerGroup m_leftDrive = new MotorControllerGroup(m_leftfrontDrive, m_leftrearDrive);
 
     private final PWMSparkMax m_rightfrontDrive = new PWMSparkMax(2);
     private final PWMSparkMax m_rightrearDrive = new PWMSparkMax(3);
-    MotorControllerGroup m_rightDrive = new MotorControllerGroup(m_rightfrontDrive, m_rightrearDrive);
+    private final MotorControllerGroup m_rightDrive = new MotorControllerGroup(m_rightfrontDrive, m_rightrearDrive);
 
     private final DifferentialDrive m_RoboDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
     private final Timer m_Timer = new Timer();
+
+   
     /*
      * The code above runs when the robot is first started up. 
      * All the intialization code goes above.
      */
-    @Override 
+    @Override
     public void RobotInit(){
         m_rightDrive.setInverted(true);
     /*
@@ -30,5 +32,9 @@ public class DriveTrain_old {
      * on how the gearbox's are set up, we might have to invert the other side
      * meaning that we change "m_rightDrive" to "m_leftDrive" and leave everything the same.
      */
+    }
+
+
+    public static void arcadeDrive(double y, double z) {
     }
 }
