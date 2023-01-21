03cb561ac2ee03e9bb5c330b39982407f503d2b2
Mike <mek8356@gmail.com>
2023-01-21 15:41:04 -0600

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -5,10 +5,23 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 
 public class Arm extends SubsystemBase {
   /** Creates a new Arm. */
-  public Arm() {}
+  public Arm() {
+    public class Arm extends SubsystemBase {
+      private Joystick m_stick;
+      private static final int deviceID = 1;
+      private CANSparkMax m_motor;
+    }    
+  }
 
   @Override
   public void periodic() {
