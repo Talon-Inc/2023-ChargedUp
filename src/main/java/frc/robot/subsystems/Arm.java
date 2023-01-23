25a410b0c222608079f6c7e7977d083c7fd8c7ab
Semaj <seoh30k@gmail.com>
2023-01-23 17:30:36 -0600

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -5,15 +5,34 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick;
+// (Arm motor import goes here) 
 
 public class Arm extends SubsystemBase {
   /** Creates a new Arm. */
   public Arm() {
-    
+    final Joystick m_Joystick; 
+
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    // set arm motor to be inverted/reversed
+    boolean reverse = true;
+    /** arm_motor */.setInverted(reverse);
+
+    // Arm limits 
+
+    /** arm_motor */.setSmartCurrentLimit(CURRENT);
+
+    // Arm idle behavior
+
+    /** arm_motor */.setIdleMode(IDLE);
+  
+    m_roboDrive = new DifferentialDrive(/** arm motor */);
+
   }
 }
