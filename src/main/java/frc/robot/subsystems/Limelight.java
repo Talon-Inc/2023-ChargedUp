a8fb619504a842a31a2eb4f87a64f605bfa664d7
Mike <110186790+MichealKing123@users.noreply.github.com>
2023-02-04 14:55:20 -0600

    Create Limelight.java

---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
+public class Limelight {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+
+
+}
