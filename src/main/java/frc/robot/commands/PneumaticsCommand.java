a25ecda223c412295cd711a94aae4e322366784b
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2023-02-04 13:28:24 -0600

    Update PneumaticsCommand.java

--- src/main/java/frc/robot/commands/PneumaticsCommand.java
+++ src/main/java/frc/robot/commands/PneumaticsCommand.java
@@ -4,21 +4,39 @@
 
 package frc.robot.commands;
 
+import static frc.robot.Constants.OperatorConstants.*;
+
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Pneumatics;
 
 public class PneumaticsCommand extends CommandBase {
+   private final Pneumatics pneumatics;
+   private XboxController controller = null;
   /** Creates a new PneumaticsCommand. */
-  public PneumaticsCommand() {
+  public PneumaticsCommand(Pneumatics subsystem, XboxController controller) {
+    this.pneumatics = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+    this.controller = controller;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    pneumatics.sol1Close();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (controller.getRawButton(BUTTON_A)){
+      pneumatics.sol1Open();
+    }
+    else if (controller.getRawButton(BUTTON_B)){
+      pneumatics.sol1Close();
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
