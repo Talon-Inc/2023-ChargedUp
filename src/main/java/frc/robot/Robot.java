6833eac24b3aede040489d6511054119e4b682bc
Mike <110186790+MichealKing123@users.noreply.github.com>
2023-02-04 14:36:07 -0600

    Removed DriveTrain_old and updated Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,17 +4,15 @@
 
 package frc.robot;
 
-import static frc.robot.Constants.OperatorConstants.*;
-import edu.wpi.first.wpilibj.Joystick;
+import static frc.robot.Constants.OperatorConstants.CONTROLLER_PORT;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.DriveTrain_old;
 import frc.robot.subsystems.Pneumatics;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
