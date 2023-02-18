1273add813b89d1065dc5916ab7a5ea13e9e1397
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2023-02-18 11:03:31 -0600

    Worked on button bindings, fixed the Middle.java and High.java commands, created a turbo and retract command, and stuff :)

--- src/main/java/frc/robot/commands/Retract.java
+++ src/main/java/frc/robot/commands/Retract.java
@@ -4,12 +4,10 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class Retract extends InstantCommand {
+public class Retract extends CommandBase {
+  /** Creates a new Retract. */
   public Retract() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -17,4 +15,18 @@ public class Retract extends InstantCommand {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
