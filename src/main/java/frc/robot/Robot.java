d6d90368428674e3994c0ac6cd5e0b06499c75a7
Mike <110186790+MichealKing123@users.noreply.github.com>
2023-02-21 17:57:42 -0600

    Remove old code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,15 +24,6 @@ public class Robot extends TimedRobot {
   
   private RobotContainer m_robotContainer;
 
-  // temp stuff
-  private final XboxController controller = new XboxController(CONTROLLER_PORT);
-  private final DriveTrain driveTrain = new DriveTrain();
-  private final Sensor sensor = new Sensor();
-  private final Limelight limelight = new Limelight();
-
-  private final Arm arm = new Arm();
-  private final Pneumatics pneumatics = new Pneumatics();
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -95,75 +86,7 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-    if (controller.getRightBumper()) { DRIVE_FACTOR = 1; }
-    else { DRIVE_FACTOR = 0.5; }
-    
-    driveTrain.arcadeDrive(DRIVE_FACTOR*controller.getLeftY(), DRIVE_FACTOR*controller.getLeftX());
-    
-    if (controller.getBackButton()){
-      pneumatics.openClaw();
-    }
-    else if (controller.getStartButton()) {
-      pneumatics.closeClaw();
-    }
-    
-    if (controller.getYButton()) {
-      arm.highLimit();
-      arm.extend(.2);
-    }
-    else if (controller.getXButton()) {
-      arm.middleLimit();
-      arm.extend(.2);
-    }
-    else if (controller.getAButton()) {
-      arm.retract(.2);
-    }
-    else {
-      arm.stop();
-    }
-    
-    System.out.println(limelight.getID());
-
-    if (controller.getBButton()) {
-      // Retrieves current yaw, pitch, and roll in spots 0, 1, 2 respectively
-      sensor.pigeonIMU.getYawPitchRoll(sensor.ypr_deg);
-      
-      //Note*//
-      //System.out.println("Yaw deg " + sensor.ypr_deg[0]);
-      //This works as intended
-
-      //System.out.println("Roll deg " + sensor.ypr_deg[1]); 
-      //Documentation states that ypr_deg[1] is Pitch but for practical purposes it is our Roll
-
-      //System.out.println("Pitch deg " + sensor.ypr_deg[2]);
-      //System.out.println(sensor.ypr_deg[2]);
-      //Documentation states that ypr_deg[2] is Roll but for practical purposes it is our Pitch
-
-      /* This code activates the auto-balance */
-
-      //This eases the movespeed according to the pitch's magnitude
-      double moveSpeed = Math.abs(sensor.ypr_deg[2])/40;
-      //This code limits the speed of the auto-balance
-      if (moveSpeed > .35){
-        moveSpeed = .35;
-      }
-
-      if (sensor.ypr_deg[2] < 2){
-        driveTrain.arcadeDrive(moveSpeed, 0);
-      }
-      else if (-2 < sensor.ypr_deg[2]) {
-        driveTrain.arcadeDrive(-moveSpeed, 0);
-      }
-      else {
-        driveTrain.arcadeDrive(0, 0);
-      }
-
-      if (controller.getLeftBumperPressed()) {
-        limelight.togglelight();
-      }
-    }
-  }
+  public void teleopPeriodic() {}
 
   @Override
   public void testInit() {
