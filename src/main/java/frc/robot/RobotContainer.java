c53c03e048725f83d978d7db22a2e5fb93b81085
Jonathan Labrum <jonathan.d.labrum@gmail.com>
2023-02-23 02:06:00 -0600

    Changed more DriveTrain to Drivetrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -89,7 +89,7 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     // return Autos.exampleAuto(m_exampleSubsystem);
-    return Autos.testAuto(driveTrain);
+    return Autos.testAuto(drivetrain);
   }
 
   /**
