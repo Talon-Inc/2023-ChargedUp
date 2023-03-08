8cca2ad166a483a8622056c9399a576db23b1ef6
Mike <110186790+MichealKing123@users.noreply.github.com>
2023-03-07 18:16:52 -0600

    Update OpenClaw.java

--- src/main/java/frc/robot/commands/OpenClaw.java
+++ src/main/java/frc/robot/commands/OpenClaw.java
@@ -4,35 +4,25 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Pneumatics;
-public class OpenClaw extends CommandBase {
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class OpenClaw extends InstantCommand {
   private Pneumatics pneumatics = null;
-  /** Creates a new OpenClaw. */
+
+  /** Create a new OpenClaw */
   public OpenClaw(Pneumatics pneumatics) {
     this.pneumatics = pneumatics;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(pneumatics);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    pneumatics.closeClaw();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    return;
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return true;
+    pneumatics.openClaw();
   }
 }
