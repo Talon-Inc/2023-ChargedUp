77e7b60d8ce806d12fe1429780f43a0c79b0e385
Mike <110186790+MichealKing123@users.noreply.github.com>
2023-03-27 17:59:22 -0500

    Update OpenClaw and CloseClaw

--- src/main/java/frc/robot/commands/CloseClaw.java
+++ src/main/java/frc/robot/commands/CloseClaw.java
@@ -7,7 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Pneumatics;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// NOTE: Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class CloseClaw extends InstantCommand {
