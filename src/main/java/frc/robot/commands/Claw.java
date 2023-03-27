4264220cc44c8072ad355a62269c44a7fd99a6fa
Mike <110186790+MichealKing123@users.noreply.github.com>
2023-03-27 18:04:31 -0500

    Update Claw.java

--- src/main/java/frc/robot/commands/Claw.java
+++ src/main/java/frc/robot/commands/Claw.java
@@ -4,52 +4,31 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.LEDIndicator;
 import frc.robot.subsystems.Pneumatics;
 
-public class Claw extends CommandBase {
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Claw extends InstantCommand {
   private Pneumatics pneumatics = null;
   private LEDIndicator ledIndicator = null;
-  private boolean flag = false;
 
-  /** 
-   * Creates a new Claw command.
-   * 
-   * @param pneumatics Gets the Pneumatics subsystem
-   */
   public Claw(Pneumatics pneumatics, LEDIndicator ledIndicator) {
     this.pneumatics = pneumatics;
-    this.ledIndicator = ledIndicator;
+
     // Use addRequirements() here to declare subsystem dependencies.
-    //addRequirements(pneumatics);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    //pneumatics.closeClaw();
+  public void initialize () {
     pneumatics.toggleclaw();
     if (pneumatics.isOpenClaw()) {
       ledIndicator.setYellow();
     } else {
       ledIndicator.setGreen();
     }
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    //pneumatics.openClaw();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }
