cec0a7c79305f946df330163e928588b5f9ede58
Mike <110186790+MichealKing123@users.noreply.github.com>
2023-04-01 12:01:33 -0500

    Update ArmControl.java

--- src/main/java/frc/robot/commands/ArmControl.java
+++ src/main/java/frc/robot/commands/ArmControl.java
@@ -25,7 +25,7 @@ public class ArmControl extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    arm.move(controller.getRightY());
+    arm.move(-controller.getRightY());
   }
 
   // Called once the command ends or is interrupted.
